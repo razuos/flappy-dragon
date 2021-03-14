eda8ae00a8f0d4dd560a3e9dae2c828f3e4fa327
Rafael Souza <rsouza19796@gmail.com>
2021-03-14 17:25:57 -0300

    fix: getKey in closed window

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from lib.graphics import GraphWin, color_rgb
+from lib.graphics import GraphicsError
 from game import Game
 
 myGame = Game(400, 700)
 
-myGame.switchState('MENU')
-
+try:
+  myGame.switchState('MENU')
+except GraphicsError as e:
+  if str(e) == "getKey in closed window":
+    quit(0)
+  else:
+    print(e)
